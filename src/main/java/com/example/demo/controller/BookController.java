6a6922cf2c478f9275ccba1cdf921a5c1db6177e
Gerard-Gon <gera.gonzalezs@duocuc.cl>
2025-10-29 00:49:56 -0300

    Implementacion patch

--- src/main/java/com/example/demo/controller/BookController.java
+++ src/main/java/com/example/demo/controller/BookController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -36,6 +37,17 @@ public class BookController {
     @Operation(summary = "Get a book by Id")
     public Book getBookById(@PathVariable Long id) {
         return bookService.getBookById(id);
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<Book> patchBook(@PathVariable Long id , @RequestBody Book parcialBook){
+        try {
+            Book updateBook = bookService.patchBook(id, parcialBook);
+            return ResponseEntity.ok(updateBook);
+            
+        } catch (RuntimeException e) {
+            return ResponseEntity.notFound().build();
+        }
     }
 
 
--- src/main/java/com/example/demo/service/BookService.java
+++ src/main/java/com/example/demo/service/BookService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 import com.example.demo.model.Book;
 import com.example.demo.repository.BookRepository;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class BookService {
@@ -27,4 +28,24 @@ public class BookService {
     public void deleteBook(Long id) {
         bookRepository.deleteById(id);
     }
+
+    public Book patchBook(Long id, Book parcialBook){
+    Optional<Book> bookOptional = bookRepository.findById(id);
+    if(bookOptional.isPresent()){
+
+        Book bookToUpdate = bookOptional.get();
+
+        if(parcialBook.getAuthor() != null){
+            bookToUpdate.setAuthor(parcialBook.getAuthor());
+        }
+        if(parcialBook.getTitle() != null){
+            bookToUpdate.setTitle(parcialBook.getTitle());
+        }
+        return bookRepository.save(bookToUpdate);
+    }else {
+        return null;
+        }
+    }
+
+
 }
